938dc31511b71da0379200f3696c624ff9308b78
andy <andy@yingyi.com.tw>
2022-08-18 15:25:52 +0800

    0818test

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {
+  console.log('callback - particles-js config loaded');
+  });
+
+particlesJS('particles-js',
+  
+'assets/js/particlesjs-config.json'
+
+);
